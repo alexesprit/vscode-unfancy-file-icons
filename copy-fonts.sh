63ac0ffdd402ecbb41d03179ecadc56668e149a5
alexesprit <alex.esprit@gmail.com>
2019-12-02 23:38:31 +0300

    Add project files

---
+++ copy-fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+octicons_font=node_modules/octicons-webfont/build/octicons.woff
+target_dir=resources/fonts
+
+mkdir -p $target_dir
+
+Echo Copy Octicons font
+cp $octicons_font $target_dir
