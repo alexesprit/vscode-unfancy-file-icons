f8946cfb5a40f38f797d9fa43b35558f0786285b
alexesprit <alex.esprit@gmail.com>
2019-12-21 15:12:50 +0300

    Improve `generate-samples` script
    Generate samples if they are missing.

--- generate-samples.js
+++ generate-samples.js
@@ -6,34 +6,72 @@ const items = require('./src/items.json');
 const samplesDir = 'samples';
 
 function main() {
-    const fileNames = getSampleFileNames();
-    createSampleFiles(fileNames);
+    const missingSamples = getMissingSamples().map((fileName) => {
+        return path.join(samplesDir, fileName);
+    });
+
+    if (missingSamples.length > 0) {
+        createSampleFiles(missingSamples);
+    } else {
+        console.log('No missing samples are found');
+    }
 }
 
-function getSampleFileNames() {
-    const { fileExtensions, fileNames } = items;
-    const sampleFileNames = {};
+function getMissingSamples() {
+    const existingSamples = getExistingSamples();
+    const sampleFiles = getFlattenSampleFiles();
+    const missingSamples = [];
 
-    for (const type in fileExtensions) {
-        const ext = fileExtensions[type][0];
-        sampleFileNames[type] = `sample.${ext}`;
-    }
+    for (const type in sampleFiles) {
+        const allSampleFiles = sampleFiles[type];
+        let isMissing = true;
 
-    for (const type in fileNames) {
-        if (type in sampleFileNames) {
-            continue;
+        for (const fileName of allSampleFiles) {
+            if (existingSamples.includes(fileName)) {
+                isMissing = false;
+            }
         }
 
-        sampleFileNames.push(fileNames[type][0]);
+        if (isMissing) {
+            missingSamples.push(allSampleFiles[0]);
+        }
     }
 
-    return Object.values(sampleFileNames).map((name) => {
-        return path.join(samplesDir, name);
-    });
+    return missingSamples;
+}
+
+function getFlattenSampleFiles() {
+    const { fileNames, fileExtensions } = items;
+    const sampleFiles = {};
+
+    for (const type in fileNames) {
+        if (!(type in sampleFiles)) {
+            sampleFiles[type] = [];
+        }
+
+        sampleFiles[type].push(...fileNames[type]);
+    }
+
+    for (const type in fileExtensions) {
+        if (!(type in sampleFiles)) {
+            sampleFiles[type] = [];
+        }
+
+        sampleFiles[type].push(...fileExtensions[type].map((ext) => {
+            return `sample.${ext}`;
+        }));
+    }
+
+    return sampleFiles;
+}
+
+function getExistingSamples() {
+    return fs.readdirSync(samplesDir);
 }
 
 function createSampleFiles(fileNames) {
     for (const fileName of fileNames) {
+        console.log(`Generate ${fileName}`);
         fs.closeSync(fs.openSync(fileName, 'w'));
     }
 }
