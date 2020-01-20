27fd3a787e6c9ae38f7c6a94981326da19ebbee9
alexesprit <alex.esprit@gmail.com>
2020-01-20 22:24:48 +0300

    Extend `check-dupes` script
    The new script file is `check-items` which checks `items.json` for duplicates and unused icon entries.
    
    Add feature to check for unused icon entries.
    Return error code on warnings.

---
+++ check-items.js
@@ -0,0 +1,87 @@
+const items = require('./src/items.json');
+
+const skipUnusedIcons = ['file', 'folder'];
+
+function main() {
+    const warnings = getWarnings();
+
+    if (warnings.length > 0) {
+        for (const warning of warnings) {
+            console.log(warning);
+        }
+        return 1;
+    }
+
+    return 0;
+}
+
+function getWarnings() {
+    const duplicates = getDuplicates();
+    const unusedIcons = getUnusedIcons();
+
+    const warnings = [];
+
+    for (const [ext, type] of duplicates) {
+        warnings.push(`Duplicate "${ext}" in "${type}" section`);
+    }
+
+    for (const iconName of unusedIcons) {
+        warnings.push(`Unused icon: ${iconName}`);
+    }
+
+    return warnings;
+}
+
+function getUnusedIcons() {
+    const { iconDefinitions, fileExtensions, fileNames } = items;
+    const unusedIcons = [];
+
+    const usedIcons = [
+        ...Object.keys(fileNames),
+        ...Object.keys(fileExtensions),
+    ];
+
+    for (const iconName in iconDefinitions) {
+        if (skipUnusedIcons.includes(iconName)) {
+            continue;
+        }
+
+        if (!usedIcons.includes(iconName)) {
+            unusedIcons.push(iconName);
+        }
+    }
+
+    return unusedIcons;
+}
+
+function getDuplicates() {
+    const { fileExtensions, fileNames } = items;
+
+    const duplicates = [
+        ...getDuplicatesInProps(fileNames),
+        ...getDuplicatesInProps(fileExtensions),
+    ];
+
+    return duplicates;
+}
+
+function getDuplicatesInProps(props) {
+    const checkedItems = [];
+    const duplicates = [];
+
+    for (const type in props) {
+        const extensions = props[type];
+
+        for (const ext of extensions) {
+            if (checkedItems.includes(ext)) {
+                duplicates.push([ext, type]);
+            } else {
+                checkedItems.push(ext);
+            }
+        }
+    }
+
+    return duplicates;
+}
+
+process.exit(main());
